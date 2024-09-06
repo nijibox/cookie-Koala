f06ecc036824b9519dfab11c5cafa6eb758d10fa
永田　愛 <manatsu_nagata@waku-2.com>
2024-09-06 14:57:12 +0900

    クッキーコアラ

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,23 +2,24 @@ import { useState } from "react"
 
 function IndexPopup() {
   const [data, setData] = useState("")
-
+  const [error, setError] = useState(null);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=d99282d065a99276&id=J001215879")
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
     <div
       style={{
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+        {error && <div>{error}</div>}
+      <button onClick={fetchData}>ボタン</button>
+      {data.toString()}
     </div>
   )
 }
